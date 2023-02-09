8a22126f18ed030417df589553e9272943008c3b
lemon <johung8545@gmail.com>
2023-02-09 14:57:15 +0800

    Python

--- day09/employee_crud.py
+++ day09/employee_crud.py
@@ -12,7 +12,7 @@ def create_employee(employee_name, employee_salary):
     args = [employee_name, employee_salary]   # 建立一個list 存放要新增的資料
     cursor = cursor.execute(sql, args)
     print('新增筆數:{}'.format(cursor.rowcount))  # 因著語句所影響想的資料筆數
-    print('最新 id 值:{}'.format(cursor.lasrrowid))
+    print('最新 id 值:{}'.format(cursor.lastrowid))
     conn.commit()
     conn.close()  # 一定要close
 
---
+++ day09/employee_test.py
@@ -0,0 +1,7 @@
+from day09.employee_crud import  create_employee
+
+# 建立資料
+if __name__ == "__main__":
+    create_employee('Jhon', 45000)
+    create_employee('Mary', 55000)
+
